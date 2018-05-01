920feecc16fc5863718504205854a706cc6a20d1
wy3 <38210249+wy3@users.noreply.github.com>
2018-05-01 16:50:56 +0700

    Update main.lua

--- Lua/main.lua
+++ Lua/main.lua
@@ -3,7 +3,6 @@ require "module"
 for pid = 1, 32768 do
 	handle = OpenProcess(0x1F0FFF, false, pid)
 	if handle then
-		SetProcessWorkingSetSizeEx(handle, -1, -1, 0x1)
 		EmptyWorkingSet(handle)
 		CloseHandle(handle)
 	end
