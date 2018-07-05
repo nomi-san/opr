edf35d30c8baed7cd5d6ec6bbbc792172f059378
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-07-05 08:18:18 +0700

    Update main.c

--- C/main.c
+++ C/main.c
@@ -11,7 +11,7 @@ typedef BOOL (__stdcall * _SetProcessWorkingSetSizeEx)(
 int main() {
 	HMODULE module = LoadLibraryA("psapi.dll");
 	HMODULE module2 = LoadLibraryA("kernel32.dll");
-	FARPROC proc = GetProcAddress(module, "EmptyWorkingSet"); // cho ai ghét thêm psapi.lib vào compiler
+	FARPROC proc = GetProcAddress(module, "EmptyWorkingSet"); // no need add psapi library
 	FARPROC proc2 = GetProcAddress(module2, "SetProcessWorkingSetSizeEx");
 	for (int i = 0; i <= 32768; ++i) {
 		HANDLE handle = OpenProcess(0x1F0FFF, 0, i);
