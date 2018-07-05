4b1c54c12f545407893865165f78dba37a57a929
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-07-05 08:59:39 +0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -2,7 +2,7 @@ from ctypes import *
 from ctypes.wintypes import *
 
 def main():
-	for pid in range(0, 32768):
+	for pid in range(1, 32768):
 		handle = windll.kernel32.OpenProcess(DWORD(0x1F0FFF), False, DWORD(pid))
 		if handle != 0:
 			windll.kernel32.SetProcessWorkingSetSizeEx(handle, c_size_t(-1), c_size_t(-1), DWORD(0x1))
